8714b67398bb514f5dc1aa7709c284fc0ed83165
Yedidia Klein <yedidia@openapp.co.il>
2017-04-01 22:01:05 +0300

    event log

---
+++ classes/event/video_view.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+/**
+ * The video_view event.
+ *
+ * @package    videostream
+ * @copyright  2017 Yedidia@openapp.co.il
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_videostream\event;
+defined('MOODLE_INTERNAL') || die();
+/**
+ * The video_view event class.
+ *
+ * @property-read array $other {
+ *      Extra information about event.
+ *
+ *      - PUT INFO HERE
+ * }
+ *
+ * @since     Moodle MOODLEVERSION
+ * @copyright 2017 Yedidia@openapp.co.il
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ **/
+class video_view extends \core\event\base {
+    protected function init() {
+        $this->data['crud'] = 'r'; // c(reate), r(ead), u(pdate), d(elete)
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'local_video';
+    }
+ 
+    public static function get_name() {
+        return get_string('eventvideo_view', 'videostream');
+    }
+ 
+    public function get_description() {
+        return "The user with id {$this->userid} viewed video id {$this->objectid}.";
+    }
+ 
+    public function get_url() {
+        return new \moodle_url('mod/videostream/view.php', array('id' => 'value'));
+    }
+ 
+    public function get_legacy_logdata() {
+        // Override if you are migrating an add_to_log() call.
+        return array($this->courseid, 'videostream', 'LOGACTION',
+            '...........',
+            $this->objectid, $this->contextinstanceid);
+    }
+ 
+    public static function get_legacy_eventname() {
+        // Override ONLY if you are migrating events_trigger() call.
+        return 'MYPLUGIN_OLD_EVENT_NAME';
+    }
+ 
+    protected function get_legacy_eventdata() {
+        // Override if you migrating events_trigger() call.
+        $data = new \stdClass();
+        $data->id = $this->objectid;
+        $data->userid = $this->relateduserid;
+        return $data;
+    }
+}

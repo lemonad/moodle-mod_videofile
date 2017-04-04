f13090c6ff38db99a4e63928e5e2cfa6d94f9452
Yedidia Klein <yedidia@openapp.co.il>
2017-04-05 00:02:31 +0300

    changed tables name for multi bit rate from dash to multi

--- renderer.php
+++ renderer.php
@@ -277,7 +277,7 @@ class mod_videostream_renderer extends plugin_renderer_base {
 		$hls_streaming = $config->hls_base_url;
 
 		$id = $videoid;
-		$streams = $DB->get_records("local_video_directory_dash",array("video_id" => $id));
+		$streams = $DB->get_records("local_video_directory_multi",array("video_id" => $id));
 		foreach ($streams as $stream) {
 			$files[]=$stream->filename;
 		}
@@ -305,7 +305,7 @@ class mod_videostream_renderer extends plugin_renderer_base {
 		$dash_streaming = $config->dash_base_url;
 		
 		$id = $videoid;
-		$streams = $DB->get_records("local_video_directory_dash",array("video_id" => $id));
+		$streams = $DB->get_records("local_video_directory_multi",array("video_id" => $id));
 		foreach ($streams as $stream) {
 			$files[]=$stream->filename;
 		}
